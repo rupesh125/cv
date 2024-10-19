a27dc31c5c16bf82c4d88ba2750e98adf4afb4af
Nikhilesh Yadav <rupeshy125@gmail.com>
2024-10-19 20:02:52 +0530

    test

--- app.py
+++ app.py
@@ -6,5 +6,17 @@ app = Flask(__name__, static_folder='.')
 def serve_index():
     return send_from_directory('.', 'index.html')
 
+@app.route('/Myhobbies.html')
+def serve_hobbies():
+    return send_from_directory('.', 'Myhobbies.html')
+
+@app.route('/ContactMe.html')
+def serve_contact():
+    return send_from_directory('.', 'ContactMe.html')
+
+@app.route('/GitNotes.html')
+def serve_git():
+    return send_from_directory('.', 'GitNotes.html')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
